d114a444960ebdb89952cd6e9140a2d191cc0880
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-07-29 16:41:26 +0900

    パーツを配置してVCに宣言する。

--- SampleUI/ViewController.swift
+++ SampleUI/ViewController.swift
@@ -7,11 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var plainButton: UIButton!
+    @IBOutlet private weak var grayButton: UIButton!
+    @IBOutlet private weak var tintedButton: UIButton!
+    @IBOutlet private weak var filledButton: UIButton!
+    @IBOutlet private weak var pullDownButton: UIButton!
+    @IBOutlet private weak var popUpButton: UIButton!
+    
+    @IBOutlet private weak var segmentControll: UISegmentedControl!
+    @IBOutlet private weak var textField: UITextField!
+    @IBOutlet private weak var slider: UISlider!
+    @IBOutlet private weak var uiSwitch: UISwitch!
+    @IBOutlet private weak var indicator: UIActivityIndicatorView!
+    @IBOutlet private weak var progressView: UIProgressView!
+    
+    @IBOutlet private weak var datePicker: UIDatePicker!
+    @IBOutlet private weak var pickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
