cc8d995b87ee198795fdf48db979d9075b1c32a9
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-08-29 09:56:37 +0900

    UI部品を一通り試してみました。

---
+++ SampleUI/SampleViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SampleViewController.swift
+//  SampleUI
+//
+//  Created by mtanaka on 2022/08/29.
+//
+
+import UIKit
+
+final class SampleViewController: UIViewController {
+    
+    
+}
--- SampleUI/ViewController.swift
+++ SampleUI/ViewController.swift
@@ -23,7 +23,6 @@ final class ViewController: UIViewController {
     @IBOutlet private weak var uiSwitch: UISwitch!
     @IBOutlet private weak var indicator: UIActivityIndicatorView!
     @IBOutlet private weak var progressView: UIProgressView!
-    
     @IBOutlet private weak var datePicker: UIDatePicker!
     @IBOutlet private weak var pickerView: UIPickerView!
     
@@ -31,7 +30,37 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
+    
+    private func reset() {
+            progressView.progress = 0.0
+        }
 
-
+        private func setProgress(progress: Float, animated: Bool) {
+            CATransaction.setCompletionBlock {
+                NSLog("finish animation")
+            }
+            progressView.setProgress(progress, animated: animated)
+            NSLog("begin animation")
+        }
+    
+    @IBAction private func tapSegmentControl(_ sender: Any) {
+        switch (sender as AnyObject).selectedSegmentIndex {
+        case 0:
+            print("サンプル１")
+            indicator.stopAnimating()
+            reset()
+        case 1:
+            print("サンプル２")
+            indicator.startAnimating()
+            setProgress(progress: 1.0, animated: true)
+        default:
+            print("該当無し")
+        }
+    }
+    
+    
+    @IBAction private func sliderAction(_ sender: UISlider) {
+        label.text = String(sender.value * 100)
+    }
 }
 
